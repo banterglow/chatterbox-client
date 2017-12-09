98069522721964e29f69a8105ef0c7b96e2cf568
wongal5 <alberthwong2@gmail.com>
2017-12-09 12:32:50 -0800

    Completed rooms and filtering

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -8,9 +8,11 @@
 
 
 let app = {
+  allChats: {},
   roomList: {},
   friendList: {},
   server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages?order=-createdAt&where=',
+  currentRoom: null,
   lastUpdate: {
     createdAt: {
       $gte: {
@@ -21,28 +23,39 @@ let app = {
   },
   init: function() {
     app.fetch();
-    setInterval(app.fetch, 3000); //interval
+    setInterval(app.fetch, 5000); //interval
+    setInterval(app.render, 2000, app.currentRoom);
     // app.addFriend();
   },
-  render: function(data) {
-    console.log(data);
-    for (var i = 0; i < data.results.length; i++) {
-      var text = _.escape(data.results[i].text);
-      var username = _.escape(data.results[i].username);
-      var timeStamp = moment(new Date(_.escape(data.results[i].createdAt))).format('LLLL');
-      var roomName = _.escape(data.results[i].roomname);
-      $('#chats').append($(`<div class="chatBody ${roomName}"><p><strong>${username}</strong><br>${text}</p><p class="timestamp">${timeStamp}</p></div>`));
+  render: function(selectedRoom) {
+    for (var chat in app.allChats) {
+      var text = _.escape(app.allChats[chat].text);
+      var username = _.escape(app.allChats[chat].username);
+      var timeStamp = moment(new Date(_.escape(app.allChats[chat].createdAt))).format('LLLL');
+      var timeElapsed = moment(new Date(_.escape(app.allChats[chat].createdAt))).fromNow();
+      var roomName = _.escape(app.allChats[chat].roomname);
+      if (roomName === selectedRoom) {
+        $('#chats').append($(`<div class="chatBody ${roomName} ${username}"><p><a class='usernameClass' href=""><strong>${username}</strong></a><br>${text}</p><p class="timestamp">${timeElapsed}</p></div>`));
+      }
+      // $('#chats').append($(`<div class="chatBody ${roomName}"><p><strong>${username}</strong><br>${text}</p><p class="timestamp">${timeStamp}</p></div>`));
     }
   },
   fetch: function() {
     // $.get(app.server, updateFeed);
-    console.log(app.server + JSON.stringify(app.lastUpdate));
+    // console.log(app.server + JSON.stringify(app.lastUpdate));
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: app.server + JSON.stringify(app.lastUpdate),
       type: 'GET',
       contentType: 'application/json',
-      success: app.render,
+      success: function(data) {
+        for (var i = 0; i < data.results.length; i++) {
+          if (_.escape(data.results[i].roomname) !== 'lobby') {
+            app.renderRoom(_.escape(data.results[i].roomname));
+            app.allChats[_.escape(data.results[i].createdAt)] = data.results[i];
+          }   
+        }
+      },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to receive message', data);
@@ -69,7 +82,8 @@ let app = {
     var message = {
       username: window.location.search.slice(10),
       text: $('#messageInput').val(),
-      roomname: $('#roomSelect').val()
+      roomname: $('#roomSelect').val(),
+      createdAt: moment(new Date(_.escape(moment().format()))).format('LLLL')
     };
     console.log(message);
     $('#messageInput').val('');
@@ -79,22 +93,32 @@ let app = {
   renderMessage: function(message) {
     app.send(JSON.stringify(message));
     //below is just for the spec test
-    $('#chats').append($(`<div class="chatBody ${message.roomname}"><p><strong> ${message.username} </strong><br> ${message.text} </p><p class="timestamp"> ${message.timeStamp} </p></div>`));
+    $('#chats').prepend($(`<div class="chatBody ${message.roomname}"><p><strong> ${message.username} </strong><br> ${message.text} </p><p class="timestamp"> ${message.createdAt} </p></div>`));
   },
   clearMessages: function() {
     $('#chats').empty();
   },
   renderRoom: function(room) {
-    $('#roomSelect').append($(`<option value="${room}">${room}</option>`));
+    if (!app.roomList[room] && room !== "") {
+      $('#roomSelect').append($(`<option value="${room}">${room}</option>`));
+      app.roomList[room] = room;
+    }
+    
   },
   createRoom: function() {
     var roomName = $('#newRoomInput').val(); 
     $('#roomSelect').append($(`<option class="${roomName}" value="${roomName}">${roomName}</option>`));
     app.enterRoom(roomName);
   },
-  enterRoom: function(roomName) {
-    var selectedRoom = $('#roomSelect').options[$('#roomSelect').roomName].value;
-    $('.roomName').attr('selected', 'selected');
+  enterRoom: function() {
+    // var selectedRoom = $('#roomSelect ${roomName} option:selected').val();
+    // console.log(selectedRoom);
+    // $('.roomName').attr('selected', 'selected');
+    var roomName = $('#roomSelect').val(); 
+    console.log(`sup ${roomName}`);
+    app.clearMessages();
+    app.currentRoom = roomName;
+    app.render(roomName);
   },
   addFriend: function() {
 
@@ -122,6 +146,12 @@ $(document).ready(function() {
   $('#submitButton').on('click', app.submitMessage);
 
   $('#createRoomButton').on('click', app.createRoom);
+
+  $('#roomSelect').on('change', app.enterRoom);
+
+  $('.usernameClass').on('click', function() {
+    console.log($(this).username);
+  });
   /* slide effect - first append it as hidden, then slide reveal it.
   var temp = '<div class="newli"><div>1</div><div>2</div><div>3</div><div>4</div></div>';
   function runEffect() {
