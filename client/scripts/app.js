155ba9acf998668e2ed50248b996b0566660a849
wongal5 <alberthwong2@gmail.com>
2017-12-08 20:04:32 -0800

    Progress on chatrooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,10 +2,11 @@
 // $(document).ready(function() {
 
 //2011-08-21T18:02:52.249Z
-//2017-12-08T17:12:74.1540-08:00 
+//2017-12-08T18:12:81.4648-08:00
+// where={"createdAt":{"$gte":{"__type":"Date","iso":"2011-08-21T18:02:52.249Z"}}}
+// where={"createdAt":{"$gte":{"__type":"Date","iso":"2017-12-08T19:12:18.249Z"}}}
 
 
-console.log(moment().format('YYYY-MM-DDTHH:MM:SS.msZ'));
 let app = {
   roomList: {},
   friendList: {},
@@ -29,11 +30,13 @@ let app = {
       var text = _.escape(data.results[i].text);
       var username = _.escape(data.results[i].username);
       var timeStamp = moment(new Date(_.escape(data.results[i].createdAt))).format('LLLL');
-      $('#chats').append($('<div class="chatBody"><p><strong>' + username + '</strong><br>' + text + '</p><p class="timestamp">' + timeStamp + '</p></div>'));
+      var roomName = _.escape(data.results[i].roomname);
+      $('#chats').append($(`<div class="chatBody ${roomName}"><p><strong>${username}</strong><br>${text}</p><p class="timestamp">${timeStamp}</p></div>`));
     }
   },
   fetch: function() {
     // $.get(app.server, updateFeed);
+    console.log(app.server + JSON.stringify(app.lastUpdate));
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: app.server + JSON.stringify(app.lastUpdate),
@@ -51,7 +54,7 @@ let app = {
       // This is the url you should use to communicate with the parse API server.
       url: app.server,
       type: 'POST',
-      data: JSON.stringify(message),
+      data: message,
       contentType: 'application/json',
       success: function (data) {
         console.log('chatterbox: Message sent');
@@ -63,28 +66,35 @@ let app = {
     });
   },
   submitMessage: function() {
-    
-  },
-  renderMessage: function() {
     var message = {
       username: window.location.search.slice(10),
       text: $('#messageInput').val(),
       roomname: $('#roomSelect').val()
     };
+    console.log(message);
     $('#messageInput').val('');
-    app.send(message);
+    app.renderMessage(message);
+  },
+  renderMessage: function(message) {
+    app.send(JSON.stringify(message));
+    //below is just for the spec test
+    $('#chats').append($(`<div class="chatBody ${message.roomname}"><p><strong> ${message.username} </strong><br> ${message.text} </p><p class="timestamp"> ${message.timeStamp} </p></div>`));
   },
   clearMessages: function() {
     $('#chats').empty();
   },
-  createRoom: function(room) {
-
+  renderRoom: function(room) {
+    $('#roomSelect').append($(`<option value="${room}">${room}</option>`));
+    
   },
-  renderRoom: function() {
-
+  createRoom: function() {
+    var roomName = $('#newRoomInput').val(); 
+    $('#roomSelect').append($(`<option class="${roomName}" value="${roomName}">${roomName}</option>`));
+    app.enterRoom(roomName);
   },
-  enterRoom: function() {
-
+  enterRoom: function(roomName) {
+    var selectedRoom = $('#roomSelect').options[$('#roomSelect').roomName].value;
+    $('.roomName').attr('selected', 'selected');
   },
   addFriend: function() {
 
@@ -109,9 +119,9 @@ app.init();
 
 
 $(document).ready(function() {
-  $('#submitButton').on('click', app.renderMessage);
+  $('#submitButton').on('click', app.submitMessage);
 
-
+  $('#createRoomButton').on('click', app.createRoom);
   /* slide effect - first append it as hidden, then slide reveal it.
   var temp = '<div class="newli"><div>1</div><div>2</div><div>3</div><div>4</div></div>';
   function runEffect() {
