f775da46e69432b725c181a9647e5b327cea1001
wongal5 <alberthwong2@gmail.com>
2017-12-09 17:46:33 -0800

    completed requirements for chatterbox

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -13,21 +13,23 @@ let app = {
   friendList: {},
   server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages?order=-createdAt&where=',
   currentRoom: null,
+  currentRoomChats: [],
   lastUpdate: {
     createdAt: {
       $gte: {
         __type: 'Date',
-        iso: moment().format('YYYY-MM-DDTHH:MM:SS') + '.249Z'
+        iso: moment().utc().format()
       }
     }
   },
   init: function() {
     app.fetch();
-    setInterval(app.fetch, 5000); //interval
-    setInterval(app.render, 2000, app.currentRoom);
+    setInterval(app.fetch, 3000); //interval
+    setInterval(app.updateRender, 2000, app.currentRoom);
     // app.addFriend();
   },
   render: function(selectedRoom) {
+    // console.log('RENDER HAS RUN' + app.lastUpdate.createdAt.$gte.iso);
     for (var chat in app.allChats) {
       var text = _.escape(app.allChats[chat].text);
       var username = _.escape(app.allChats[chat].username);
@@ -35,9 +37,21 @@ let app = {
       var timeElapsed = moment(new Date(_.escape(app.allChats[chat].createdAt))).fromNow();
       var roomName = _.escape(app.allChats[chat].roomname);
       if (roomName === selectedRoom) {
-        $('#chats').append($(`<div class="chatBody ${roomName} ${username}"><p><a class='usernameClass' href=""><strong>${username}</strong></a><br>${text}</p><p class="timestamp">${timeElapsed}</p></div>`));
+        $('#chats').append($(`<div class="chatBody ${roomName} ${username}"><div class='usernameClass'><strong>${message.username}</strong></div><p class="messageText">${message.text}</p><p class="timestamp">${timeElapsed}</p></div>`));
       }
       // $('#chats').append($(`<div class="chatBody ${roomName}"><p><strong>${username}</strong><br>${text}</p><p class="timestamp">${timeStamp}</p></div>`));
+    }
+  },
+  updateRender: function(selectedRoom) {
+    while (app.currentRoomChats.length > 0) {
+      var text = _.escape(app.currentRoomChats[0].text);
+      var username = _.escape(app.currentRoomChats[0].username);
+      var timeStamp = moment(new Date(_.escape(app.currentRoomChats[0].createdAt))).format('LLLL');
+      var timeElapsed = moment(new Date(_.escape(app.currentRoomChats[0].createdAt))).fromNow();
+      console.log(`update render ${timeElapsed}`);
+      var roomName = _.escape(app.currentRoomChats[0].roomname);
+      $('#chats').prepend($(`<div class="chatBody ${roomName} ${username}"><div class='usernameClass'><strong>${username}</strong></div><p class="messageText">${text}</p><p class="timestamp">${timeElapsed}</p></div>`));
+      app.currentRoomChats.shift();
     }
   },
   fetch: function() {
@@ -49,12 +63,17 @@ let app = {
       type: 'GET',
       contentType: 'application/json',
       success: function(data) {
+        app.lastUpdate.createdAt.$gte.iso = moment().utc().format();
         console.log(data);
-        app.lastUpdate.createdAt.$gte.iso = moment().format('YYYY-MM-DDTHH:MM:SS') + '.249Z';
         for (var i = 0; i < data.results.length; i++) {
-          if (_.escape(data.results[i].roomname) !== 'lobby') {
+          if (_.escape(data.results[i].username) !== 'Chatterberts') {
+            // renders new rooms based on incoming messages
             app.renderRoom(_.escape(data.results[i].roomname));
+            // adds all messages to allChat object
             app.allChats[_.escape(data.results[i].createdAt)] = data.results[i];
+            if (data.results[i].roomname === app.currentRoom) {
+              app.currentRoomChats.push(data.results[i]);
+            }
           }   
         }
       },
@@ -80,7 +99,7 @@ let app = {
       }
     });
   },
-  submitMessage: function() {
+  handleSubmit: function() {
     var message = {
       username: window.location.search.slice(10),
       text: $('#messageInput').val(),
@@ -95,7 +114,8 @@ let app = {
   renderMessage: function(message) {
     app.send(JSON.stringify(message));
     //below is just for the spec test
-    $('#chats').prepend($(`<div class="chatBody ${message.roomname}"><p><strong> ${message.username} </strong><br> ${message.text} </p><p class="timestamp"> ${message.createdAt} </p></div>`));
+    var timeElapsed = moment(new Date(_.escape(message.createdAt))).fromNow();
+    $('#chats').prepend($(`<div class="chatBody ${message.roomname} ${message.username}"><div class='usernameClass'><strong>${message.username}</strong></div><p class="messageText">${message.text}</p><p class="timestamp">${timeElapsed}</p></div>`));
   },
   clearMessages: function() {
     $('#chats').empty();
@@ -109,6 +129,7 @@ let app = {
   },
   createRoom: function() {
     var roomName = $('#newRoomInput').val(); 
+    $('#newRoomInput').val('');
     $('#roomSelect').append($(`<option class="${roomName}" value="${roomName}">${roomName}</option>`));
     app.enterRoom(roomName);
   },
@@ -117,13 +138,17 @@ let app = {
     // console.log(selectedRoom);
     // $('.roomName').attr('selected', 'selected');
     var roomName = $('#roomSelect').val(); 
-    console.log(`sup ${roomName}`);
+    console.log(`entered ${roomName}`);
     app.clearMessages();
     app.currentRoom = roomName;
     app.render(roomName);
   },
-  addFriend: function() {
-
+  addFriend: function(username) {
+    app.friendList[username] = username;
+    $('#chats').css('cursor', 'pointer');
+    // $(`.${username}`).addClass('friend');
+    let styleFriend = $(`<style>.${username} { border-color: black; color: #8258FA; }</style>`);
+    $('body').append(styleFriend);
   }
 };
 
@@ -145,15 +170,30 @@ app.init();
 
 
 $(document).ready(function() {
-  $('#submitButton').on('click', app.submitMessage);
+  $('#submitButton').on('click', app.handleSubmit);
+
+  $('#messageInput').keypress(function(e) {
+    if (e.keyCode == 13) {
+      $('#submitButton').trigger('click');
+    }
+  });
+
+  // $('#messageInput').on('keypress', app.handleSubmit);
 
   $('#createRoomButton').on('click', app.createRoom);
 
+  $('#newRoomInput').keypress(function(e) {
+    if (e.keyCode == 13) {
+      $('#createRoomButton').trigger('click');
+    }
+  });
+
   $('#roomSelect').on('change', app.enterRoom);
 
-  $('.usernameClass').on('click', function() {
-    console.log($(this).username);
-  });
+  $('#chats').on('click', 'div.usernameClass', function() {
+    let username = $(this).text();
+    app.addFriend(username);
+  }); 
   /* slide effect - first append it as hidden, then slide reveal it.
   var temp = '<div class="newli"><div>1</div><div>2</div><div>3</div><div>4</div></div>';
   function runEffect() {
